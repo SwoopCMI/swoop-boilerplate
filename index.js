dcd575905f11a723567cba3a01a2cac35ea1006e
dzannotti <d.zannotti@me.com>
2015-06-27 17:28:54 +0100

    added default target to spec

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ module.exports = function (opts) {
             hash: false,
             publicPath: "/"
         }),
+        target: "browser",
         configFile: null,
         isDev: true,
         package: null,
